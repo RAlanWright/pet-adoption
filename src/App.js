268a7dd51448f09fd57f6af08f75a52fbdc78ac3
RAlanWright <wright.alan88@gmail.com>
2019-11-08 12:58:58 -0600

    Ran npm init and installed/configured prettier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div", // What kind of element?
-        { id: "Example attribute" }, // Empty object; The attributes of the component
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Conan",
-                animal: "Cat",
-                breed: "Medium-Hair Gray"
-            }),
-            React.createElement(Pet, {
-                name: "Seestah",
-                animal: "Cat",
-                breed: "Medium-Hair Gray"
-            }),
-            React.createElement(Pet, {
-                name: "Road-Roller",
-                animal: "Cat",
-                breed: "Medium-Hair Black"
-            })
-        ]
-    );
+  return React.createElement(
+    "div", // What kind of element?
+    { id: "Example attribute" }, // Empty object; The attributes of the component
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Conan",
+        animal: "Cat",
+        breed: "Medium-Hair Gray"
+      }),
+      React.createElement(Pet, {
+        name: "Seestah",
+        animal: "Cat",
+        breed: "Medium-Hair Gray"
+      }),
+      React.createElement(Pet, {
+        name: "Road-Roller",
+        animal: "Cat",
+        breed: "Medium-Hair Black"
+      })
+    ]
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
