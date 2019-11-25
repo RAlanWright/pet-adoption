af578f229a073b923d1269d61984e85483be5ff1
RAlanWright <wright.alan88@gmail.com>
2019-11-25 16:32:40 -0600

    Removed comments from App.js, changed render to use <App/>

--- src/App.js
+++ src/App.js
@@ -3,29 +3,6 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  // return React.createElement(
-  //   "div", // What kind of element?
-  //   { id: "Example attribute" }, // Empty object; The attributes of the component
-  //   [
-  //     React.createElement("h1", { id: "something-important" }, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Conan",
-  //       animal: "Cat",
-  //       breed: "Medium-Hair Gray"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Seestah",
-  //       animal: "Cat",
-  //       breed: "Medium-Hair Gray"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Road-Roller",
-  //       animal: "Cat",
-  //       breed: "Medium-Hair Black"
-  //     })
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
@@ -36,4 +13,4 @@ const App = () => {
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
