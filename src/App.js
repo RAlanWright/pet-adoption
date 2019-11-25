90d9697da052570f8f31332218ccc5453c6fb764
RAlanWright <wright.alan88@gmail.com>
2019-11-25 16:30:21 -0600

    Changed App.js and Pet.js to use jsx formatting; Fixed errors resulting from eslint not detecting React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div", // What kind of element?
-    { id: "Example attribute" }, // Empty object; The attributes of the component
-    [
-      React.createElement("h1", { id: "something-important" }, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Conan",
-        animal: "Cat",
-        breed: "Medium-Hair Gray"
-      }),
-      React.createElement(Pet, {
-        name: "Seestah",
-        animal: "Cat",
-        breed: "Medium-Hair Gray"
-      }),
-      React.createElement(Pet, {
-        name: "Road-Roller",
-        animal: "Cat",
-        breed: "Medium-Hair Black"
-      })
-    ]
+  // return React.createElement(
+  //   "div", // What kind of element?
+  //   { id: "Example attribute" }, // Empty object; The attributes of the component
+  //   [
+  //     React.createElement("h1", { id: "something-important" }, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Conan",
+  //       animal: "Cat",
+  //       breed: "Medium-Hair Gray"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Seestah",
+  //       animal: "Cat",
+  //       breed: "Medium-Hair Gray"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Road-Roller",
+  //       animal: "Cat",
+  //       breed: "Medium-Hair Black"
+  //     })
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Conan" animal="Cat" breed="Medium-Hair Gray" />
+      <Pet name="Seestah" animal="Cat" breed="Medium-Hair Gray" />
+      <Pet name="Road-Roller" animal="Cat" breed="Medium-Hair Black" />
+    </div>
   );
 };
 
