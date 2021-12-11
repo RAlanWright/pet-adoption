cc9f81ebfe568fc011aae431bacf3c12140b98ea
Alan <wright.alan88@gmail.com>
2021-12-11 16:49:34 -0600

    Added StrictMode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { render } from "react-dom";
-import SearchParams from "./SearchParams";
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	);
 };
 
-render(<App />, document.getElementById("root"));
+render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	document.getElementById('root')
+);
